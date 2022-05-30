a1abfca51b6088b0f317a9629cfff109318b231a
smie8 <sami.eriksson@yahoo.fi>
2022-05-30 13:17:12 +0300

    Complete assignment.

--- src/main/java/guru/springframework/sfgdi/services/GreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/GreetingService.java
@@ -1,8 +1,11 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.stereotype.Service;
+
 /**
  * Created by jt on 12/26/19.
  */
+@Service("i18nService")
 public interface GreetingService {
 
     String sayGreeting();
